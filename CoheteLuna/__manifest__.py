aac5b01b75a92f8c459e6bf25ca47055b95b0f24
JanuaryG <odoo@ampd1.odoo.com>
2021-11-10 01:19:45 +0000

    [ADD] Basic Module

---
+++ CoheteLuna/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Cohete Odoo',
+    
+    'summary': """Cohete para la luna""",
+    
+    'description': """Aplicación para organizar la logística. Esto incluye la nave espacial y la tripulación.""",
+    
+    'author': 'January Gómez',
+    
+    'category': 'Formación',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
